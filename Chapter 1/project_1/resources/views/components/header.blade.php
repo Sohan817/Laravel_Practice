3067fd1f424f4cd5c13d7e3fe208bb1b6b7d2f6c
Sohan817 <hsohan509@gmail.com>
2023-08-15 09:30:03 +0600

    Routing, Controller, Component, View

---
+++ Chapter 1/project_1/resources/views/components/header.blade.php
@@ -0,0 +1,5 @@
+
+<div>
+<h1>{{$title}} 's Header Components</h1>
+    <!-- Breathing in, I calm body and mind. Breathing out, I smile. - Thich Nhat Hanh -->
+</div>
---
+++ Chapter 1/project_1/resources/views/welcome.blade.php
@@ -0,0 +1,3 @@
+<h1>Welcome Page</h1>
+<a href="/about">About Page</a>
+<a href="/contact">Contact Page</a>
